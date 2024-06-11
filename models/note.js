174b8768f22872d653cd12d33372352874a8d297
Antonio-Munoz <antonio.dev28@gmail.com>
2024-06-11 11:40:18 -0700

    Fixing bd url error

--- models/note.js
+++ models/note.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url).then(result => {
+mongoose.connect(url.toString()).then(result => {
     console.log('connected to MongoDB')
 })
 .catch(error => {
